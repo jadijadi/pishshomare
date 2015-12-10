4e3bfd2388c120e1b8292050d3284f7319069bcd
Masoud Mirzaei <mamos98@gmail.com>
2015-12-10 17:02:27 +0330

    Changing UX and UI Design

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // particlesJS
+  particlesJS.load('particles-js','js/particles.json');
+  //Sticky Menu
+  $("#sticky").sticky({ topSpacing: 0 });
+  // Canvas Height Change
+  $("canvas").attr({"height":"200"});
+});
